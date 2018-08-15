fafacfdc11b25319beed0ae3862bd0f4f39cdb51
amany9000 <201651007@iiitvadodara.ac.in>
2018-08-15 11:51:13 +0530

    Added contracts/lottery.sol

---
+++ contracts/lottery.sol
@@ -0,0 +1,17 @@
+
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    constructor() public {
+        manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        require (msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    
+} 
